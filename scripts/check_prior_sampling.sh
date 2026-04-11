0f13e646820f8b886e93b9670ed16f684e336bcf
Jamie R. Oaks <joaks1@gmail.com>
2026-04-10 21:40:37 -0500

    Adding shell script to run check_prior_sampling.py.

---
+++ scripts/check_prior_sampling.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+set -e
+
+micromamba run -n hyper-time python check_prior_sampling.py \
+    --seed 123
+    --ecoevolity-dir ../bin \
+    --number-of-runs 20 \
+    --number-of-procs 8 \
+    --output-dir ../prior-sampling-output \
+    ../ecoevolity-configs/dpp-conc-hypergamma-4-10-time-hyperexp-01-pairs-20-sites-20000.yml 1>check_prior_sampling.sh.out 2>&1
