bf712fe44e7e9987250f071374f39b92b748003a
Jamie R. Oaks <joaks1@gmail.com>
2026-05-13 16:29:34 -0500

    Updatin check_prior_sampling.sh.
    Updating to use pycoevolity rather than project scripts (which have been
    ported to pycoevolity and removed).

--- scripts/check_prior_sampling.sh
+++ scripts/check_prior_sampling.sh
@@ -4,14 +4,20 @@ set -e
 
 source ../project_utils.sh
 
+# The uniform prior on divergence times with a uniform hyper prior on the max
+# parameter is difficult to sample with MCMC without any data, so we need a
+# large burnin and thinning of the MCMC samples.
 for config_path in ../ecoevolity-configs/dpp-conc-hypergamma-4-10-time-hyper*.yml
 do
     echo "Checking prior sampling for '$(basename $config_path)'..."
-    "$ht_conda_exe" run -n hyper-time python check_prior_sampling.py \
+    "$ht_conda_exe" run -n hyper-time pyco-eco-vet-prior \
         --seed 123 \
         --ecoevolity-dir ../bin \
-        --number-of-runs 20 \
+        --number-of-runs 400 \
         --number-of-procs 8 \
+        --burnin 801 \
+        --step 6 \
+        --sparse-pop-size-plotting \
         --output-dir ../prior-sampling-output \
         "$config_path"
 done 1>check_prior_sampling.sh.out 2>&1
