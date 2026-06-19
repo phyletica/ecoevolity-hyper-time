af7e8b7a9e0fe182f30ff1a4b130e01b36fd907f
Jamie R. Oaks <joaks1@gmail.com>
2026-06-19 14:32:18 -0500

    Adding bash scripts for ecoevolity gecko runs.

---
+++ scripts/gecko-eco-runs/geckos-combined-hyper-time-run-03.sh
@@ -0,0 +1,40 @@
+#! /bin/bash
+
+set -e
+
+project_dir="../.."
+
+source "${project_dir}/project_utils.sh"
+
+run=03
+nthreads=1
+config_file_prefix="geckos-combined-hyper-time"
+
+exe_path="${project_dir}/bin/ecoevolity"
+config_path="${project_dir}/ecoevolity-configs/${config_file_prefix}.yml"
+
+if [ ! -x "$exe_path" ]
+then
+    echo "ERROR: No executable '${exe_path}'."
+    echo "       You probably need to run the project setup script."
+    exit 1
+fi
+
+output_dir="${project_dir}/gecko-ecoevolity-output"
+
+if [ ! -e "$output_dir" ]
+then
+    mkdir "$output_dir"
+fi
+
+prefix="${output_dir}/run-${run}-threads-${nthreads}-"
+out_path="${prefix}${config_file_prefix}.out"
+
+"$ht_conda_exe" run -n "$ht_conda_env_name" "$exe_path" \
+    --seed $run \
+    --nthreads "$nthreads" \
+    --prefix "$prefix" \
+    --relax-missing-sites \
+    --relax-constant-sites \
+    --relax-triallelic-sites \
+    "$config_path" 1>"$out_path" 2>&1
