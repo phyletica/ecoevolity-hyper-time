032cb2ed4f13604703ccc7d57538de9e424408ee
Jamie R. Oaks <joaks1@gmail.com>
2026-07-27 17:28:01 -0500

    Updating SVG grid python script.
    Adding loop over dict to reduce code redundancy. Also, adding the
    creation of two additional plot grids. Adding these 2 SVG plot grids
    too.

--- scripts/create_svg_grid_sim_nevents.py
+++ scripts/create_svg_grid_sim_nevents.py
@@ -17,47 +17,42 @@ def main():
         "images",
     )
 
-    unif_svg_files = (
-        "unif-hp-sim-study-nevents-heatmap-grid.svg",
-        "unif-hp-sim-study-nevents-error-scatter-grid.svg",
-    )
-    unif_svg_paths = tuple(os.path.join(image_dir_path, f) for f in unif_svg_files)
-    out_path = os.path.join(
-        image_dir_path,
-        "unif-hp-sim-study-nevents-grid.svg",
-    )
+    files_to_out_file = {
+        (
+            "unif-hp-sim-study-nevents-heatmap-grid.svg",
+            "unif-hp-sim-study-nevents-error-scatter-grid.svg",
+        ) : "unif-hp-sim-study-nevents-grid.svg",
+        (
+            "exp-hp-sim-study-nevents-heatmap-grid.svg",
+            "exp-hp-sim-study-nevents-error-scatter-grid.svg",
+        ) : "exp-hp-sim-study-nevents-grid.svg",
+        (
+            "exp-hp-sim-study-nevents-heatmap-grid.svg",
+            "unif-hp-sim-study-nevents-heatmap-grid.svg",
+        ) : "hp-sim-study-nevents-grid.svg",
+        (
+            "exp-hp-sim-study-nevents-error-scatter-grid.svg",
+            "unif-hp-sim-study-nevents-error-scatter-grid.svg",
+        ) : "hp-sim-study-nevents-error-grid.svg",
+    }
 
-    fig = svg_helpers.stack_plots(
-        svg_paths = unif_svg_paths,
-        space = 0,
-        scale_by_width = True,
-        include_letters = False,
-        letter_font_weight = "bold",
-        letter_font_size = 18,
-        letter_x_indent = None,
-    )
-    fig.save(out_path)
+    for svg_files, out_file in files_to_out_file.items():
+        svg_paths = tuple(os.path.join(image_dir_path, f) for f in svg_files)
+        out_path = os.path.join(
+            image_dir_path,
+            out_file,
+        )
 
-    exp_svg_files = (
-        "exp-hp-sim-study-nevents-heatmap-grid.svg",
-        "exp-hp-sim-study-nevents-error-scatter-grid.svg",
-    )
-    exp_svg_paths = tuple(os.path.join(image_dir_path, f) for f in exp_svg_files)
-    out_path = os.path.join(
-        image_dir_path,
-        "exp-hp-sim-study-nevents-grid.svg",
-    )
-
-    fig = svg_helpers.stack_plots(
-        svg_paths = exp_svg_paths,
-        space = 0,
-        scale_by_width = True,
-        include_letters = False,
-        letter_font_weight = "bold",
-        letter_font_size = 18,
-        letter_x_indent = None,
-    )
-    fig.save(out_path)
+        fig = svg_helpers.stack_plots(
+            svg_paths = svg_paths,
+            space = 0,
+            scale_by_width = True,
+            include_letters = False,
+            letter_font_weight = "bold",
+            letter_font_size = 18,
+            letter_x_indent = None,
+        )
+        fig.save(out_path)
 
 if __name__ == '__main__':
     main()
